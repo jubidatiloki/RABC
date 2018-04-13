dd49812036f296cf0867ea5f1805993efe15a776
benji <benja@164.81.232.131>
2018-04-13 10:26:52 +0200

    modif onPressed

--- app/src/main/java/procorp/rabc/MainActivity.java
+++ app/src/main/java/procorp/rabc/MainActivity.java
@@ -259,7 +259,7 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            //super.onBackPressed();
         }
     }
 
